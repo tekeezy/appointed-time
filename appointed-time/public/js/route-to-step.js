adf702b5ec762a19c7db2602e752fd8f44945153
Gyeong-Sik Park <tpnet3@gmail.com>
2017-09-20 11:01:07 +0900

    step 조건 추가

--- appointed-time/public/js/route-to-step.js
+++ appointed-time/public/js/route-to-step.js
@@ -1,5 +1,6 @@
 (function() {
-  if (localStorage.getItem("joined") && CURRENT_STEP < 3) {
+  if (localStorage.getItem("joined") && CURRENT_STEP != 3
+        && ! localStorage.getItem("checked_time")) {
     window.location = "./step-3.html";
   }
 })();
